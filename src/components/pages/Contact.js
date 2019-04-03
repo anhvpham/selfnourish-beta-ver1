3f48622d20bc2f6f9abbb310dd3e4208f6416c62
Vu Anh Pham <41308243+anhvpham@users.noreply.github.com>
2019-04-03 02:22:44 -0700

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react"
  import { withStyles } from "@material-ui/core/styles"
  import Typography from "@material-ui/core/Typography"
- import { Divider } from "@material-ui/core";
+
  import Button from "@material-ui/core/Button"
- import { withRouter } from "react-router-dom"
+
 
  
 
  import ContactUs from "../parts/ContactUs"
- import Header from "../../components/containers/Header"
-import Footer from "../../components/containers/Footer"      
+     
     
  const styles = theme => ({
     root:{
-        marginTop:"50px"
+        marginTop:"50px",
+        
+
+        
     },
     
     card:{
@@ -34,21 +36,25 @@ import Footer from "../../components/containers/Footer"
        
         
        <div className="contact">
-        <Header />
+       
         <ContactUs
         header_2="We'd love to hear from you"
         para_1="Thank you for your interest to SelfNourish! if you would like to keep up to date about SelfNourish, please fill in the form below"
         id="Contact us"
         />
 
-        <Footer />
+    
 
         </div>
        
     )
  }
 
- export default Contact
+ export default withStyles(styles) (Contact)
+
+
+
+ 
 
 
 
