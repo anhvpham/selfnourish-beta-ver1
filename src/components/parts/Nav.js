888644dc0367c73947ee0a0de55fb81fd202b43c
Vu Anh Pham <41308243+anhvpham@users.noreply.github.com>
2019-04-05 23:43:22 -0700

    Update Nav.js

--- src/components/parts/Nav.js
+++ src/components/parts/Nav.js
@@ -14,7 +14,7 @@ import LogoMobile from "../../assets/logo/logo_mobile.svg"
 
 const styles = theme => ({
   root: {
-    maxWidth: "75rem",
+    maxWidth: "80rem",
     backgroundColor:"transparent",
     margin: "0px auto",
     display: "flex",
