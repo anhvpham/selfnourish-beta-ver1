324a0d97672089c1d993b72350a2a4c24c0d1cfd
Vu Anh Pham <41308243+anhvpham@users.noreply.github.com>
2019-04-09 23:46:50 -0700

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@
  import Button from "@material-ui/core/Button"
  import SvgIcon from '@material-ui/core/SvgIcon'
 
-
+//import parts
  import Hero from "../parts/Hero"
 import Features from "../parts/Features"
 import HowItWorks from "../parts/HowItWorks"
